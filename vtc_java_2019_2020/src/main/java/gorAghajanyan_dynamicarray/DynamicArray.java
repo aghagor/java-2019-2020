33477d93e2fd1db01b696a17135cc737c97e3a1c
Goro <aghagor@gmail.com>
2019-11-07 00:49:06 +0400

    - Commited 'Dynamic Array' implementation

---
+++ vtc_java_2019_2020/src/main/java/gorAghajanyan_dynamicarray/DynamicArray.java
@@ -0,0 +1,112 @@
+package gorAghajanyan_dynamicarray;
+
+
+public class DynamicArray {
+    
+    private int[] storage;
+    private int size;
+
+    public DynamicArray(int capacity) {
+        this.storage = new int[capacity];
+        this.size = 0;
+    }
+
+    //returns count of elements added in to DynamicArray
+    public int size() {
+        if (size < 0) {
+            System.out.println("Array size can't be negative!!!");
+            return -1;
+        }
+        return size;
+    }
+
+    //return true if is empty
+    public boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // returns true if the value is exist in the Dynamicarray 
+    public boolean contains(int val) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i] == val) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // returns the element by specified index 
+    public int get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " +
+                    index + ", size: " + size);
+        }
+        return storage[index];
+    }
+
+    // sets the specified val by the specified index
+    public void set(int index, int val) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " +
+                    index + ", size: " + size);
+        }
+        storage[index] = val;
+    }
+
+    /**
+     * adds the element at the end position in this DynamicArray .
+     */
+    public void add(int val) {
+        if (size == storage.length - 1) {
+            extendStorage();
+        }
+        storage[size++] = val;
+    }
+
+    /**
+     * adds the element at the specified position in this DynamicArray .
+     * Shifts any subsequent elements to the right
+     */
+    public void add(int index, int element) {
+        if (size == storage.length) {
+            extendStorage();
+        }
+        for (int i = size; i > index; i--) {
+            storage[i] = storage[i - 1];
+        }
+        storage[index] = element;
+        size++;
+    }
+
+    /**
+     * Removes the element at the specified position in this DynamicArray .
+     * Shifts any subsequent elements to the left
+     */
+    public void remove(int index) {
+        if (index >= size || index < 0 || isEmpty()) {
+            throw new IndexOutOfBoundsException("Index: " +
+                    index + ", Size: " + size);
+        }
+
+        for (int i = index; i < size; i++) {
+            storage[i] = storage[i + 1];
+        }
+        size--;
+    }
+
+    public void extendStorage() {
+        int[] newStorage = new int[size * 3 / 2 + 1];
+        for (int i = 0; i < storage.length; i++) {
+            newStorage[i] = storage[i];
+        }
+        storage = newStorage;
+    }
+
+    public void empty() {
+        size = 0;
+    }
+}
---
+++ vtc_java_2019_2020/src/main/java/gorAghajanyan_dynamicarray/DynamicArrayTest.java
@@ -0,0 +1,145 @@
+package gorAghajanyan_dynamicarray;
+
+
+import java.util.Random;
+
+public class DynamicArrayTest {
+    private static DynamicArray dynamicArray;
+    private static Random random;
+
+    public static void main(String[] args) {
+
+        initDynamicArray();
+
+        emptyTest();
+
+        sizeTest();
+
+        addTest();
+
+        getTest();
+
+        isEmptyTest();
+
+        setTest();
+
+        addByIndexTest();
+
+        removeTest();
+
+        containsTest();
+
+    }
+
+    public static void initDynamicArray() {
+        dynamicArray = new DynamicArray(30);
+        random = new Random();
+    }
+
+    public static void emptyTest() {
+        dynamicArray.add(45);
+        dynamicArray.empty();
+        if (dynamicArray.size() != 0) {
+            System.err.println("The 'empty' test is not passed!");
+            return;
+        }
+        System.out.println("The 'empty' test is passed.");
+    }
+
+    public static void sizeTest() {
+        dynamicArray.empty();
+        int count = 5;
+        for (int i = 0; i < count; i++) {
+            dynamicArray.add(i);
+        }
+        if (dynamicArray.size() != count) {
+            System.err.println("The 'size' test is not passed!");
+            return;
+        }
+        System.out.println("The 'size' test is passed.");
+    }
+
+    public static void addTest() {
+        int value = random.nextInt();
+        int size = dynamicArray.size();
+        dynamicArray.add(value);
+        if (dynamicArray.size() != size + 1 || dynamicArray.get(size) != value) {
+            System.err.println("The 'add' test is not passed!");
+            return;
+        }
+        System.out.println("The 'add' test is passed!");
+    }
+
+    public static void getTest() {
+        int value = random.nextInt();
+        int size = dynamicArray.size();
+        dynamicArray.add(value);
+        if (dynamicArray.size() != size + 1 || dynamicArray.get(size) != value) {
+            System.err.println("The 'get' test is not passed!");
+            return;
+        }
+        System.out.println("The 'get' test is passed!");
+    }
+
+    public static void isEmptyTest() {
+        dynamicArray.empty();
+        if (!dynamicArray.isEmpty()) {
+            System.err.println("The 'isEmpty' test is not passed!");
+            return;
+        }
+        System.out.println("The 'isEmpty' test is passed.");
+    }
+
+    public static void setTest() {
+        int count = 5;
+        for (int i = 0; i < count; i++) {
+            dynamicArray.add(random.nextInt());
+        }
+        int value = -854;
+        dynamicArray.set(2, value);
+        if (dynamicArray.get(2) != value) {
+            System.err.println("The 'set' test is not passed!");
+            return;
+        }
+        System.out.println("The 'set' test is passed.");
+
+    }
+
+    public static void addByIndexTest() {
+        dynamicArray.add(random.nextInt());
+        int size = dynamicArray.size();
+        int value = random.nextInt();
+        int index = 1;
+        dynamicArray.add(index, value);
+        if (dynamicArray.get(index) != value || dynamicArray.size() != size + 1) {
+            System.err.println("The 'add by index' test is not passed!");
+            return;
+        }
+        System.out.println("The 'add by index' test is passed.");
+
+    }
+
+    public static void removeTest() {
+        dynamicArray.add(random.nextInt());
+        dynamicArray.add(random.nextInt());
+        int size = dynamicArray.size();
+
+        dynamicArray.remove(1);
+        if (dynamicArray.size() + 1 != size) {
+            System.err.println("The 'remove test is not passed!");
+            return;
+        }
+        System.out.println("The 'remove' test is passed.");
+    }
+
+    public static void containsTest() {
+        int value = random.nextInt();
+        dynamicArray.add(value);
+        if (!dynamicArray.contains(value)) {
+            System.err.println("The 'contains' test is not passed!");
+            return;
+        }
+        System.out.println("The 'contains' test is passed.");
+    }
+
+}
